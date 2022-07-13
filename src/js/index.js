b8d7bf792cbdc3a384e0467076081141044f8e35
Roberto Dias <roberto.rhd@gmail.com>
2022-07-13 10:36:02 -0300

    melhorias de código

---
+++ src/js/index.js
@@ -0,0 +1,32 @@
+const imagens = document.querySelectorAll(".cartao");
+const setaVoltar = document.getElementById("btn-voltar");
+const setaAvancar = document.getElementById("btn-avancar");
+var imagemAtual = 0;
+
+function esconderImagens() {
+  imagens.forEach((imagem) => {
+    imagem.classList.remove("selecionado");
+  });
+}
+
+function selecionarImagem() {
+  imagens[imagemAtual].classList.add("selecionado");
+}
+
+setaAvancar.addEventListener("click", function () {
+  if (imagemAtual !== imagens.length - 1) {
+    imagemAtual++;
+  }
+
+  esconderImagens();
+  selecionarImagem();
+});
+
+setaVoltar.addEventListener("click", function () {
+  if (imagemAtual !== 0) {
+    imagemAtual--;
+  }
+
+  esconderImagens();
+  selecionarImagem();
+});
