f1de71aa921bb218113a5654ba23f74f5ec81b47
devemdobro <devemdobro@gmail.com>
2022-07-30 17:26:00 -0300

    melhorias nomes das funções

--- src/js/index.js
+++ src/js/index.js
@@ -3,29 +3,29 @@ const btnAvancar = document.getElementById("btn-avancar");
 const btnVoltar = document.getElementById("btn-voltar");
 let cartaoAtual = 0;
 
-function esconderCartoes() {
+function esconderCartaoSelecionado() {
   const cartaoSelecionado = document.querySelector(".selecionado");
   cartaoSelecionado.classList.remove("selecionado");
 }
 
-function mostrarCartao() {
-  cartoes[cartaoAtual].classList.add("selecionado");
+function mostrarCartao(indiceCartaoPraMostrar) {
+  cartoes[indiceCartaoPraMostrar].classList.add("selecionado");
 }
 
 btnAvancar.addEventListener("click", function () {
   if (cartaoAtual === cartoes.length - 1) return;
 
-  esconderCartoes();
+  esconderCartaoSelecionado();
 
   cartaoAtual++;
-  mostrarCartao();
+  mostrarCartao(cartaoAtual);
 });
 
 btnVoltar.addEventListener("click", function () {
   if (cartaoAtual === 0) return;
 
-  esconderCartoes();
+  esconderCartaoSelecionado();
 
   cartaoAtual--;
-  mostrarCartao();
+  mostrarCartao(cartaoAtual);
 });
